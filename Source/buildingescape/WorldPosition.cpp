7021d02cabccaaab345cc3c5a65a52a318fcf9eb
Omur Ozbahceliler <omur.ozbahceliler@unity3d.com>
2020-05-14 23:43:20 +0100

    basic project setup

---
+++ Source/buildingescape/WorldPosition.cpp
@@ -0,0 +1,36 @@
+// Testing purposes only I guess...
+
+
+#include "WorldPosition.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("This is a warning!"));
+	UE_LOG(LogTemp, Error, TEXT("ERROR!"));
+	UE_LOG(LogTemp, Display, TEXT("This is just a message"));
+	
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
